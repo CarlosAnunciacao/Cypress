f4979a11dab52f01908be28df8c479e5d147941e
Carlos Anunciacao <dev.carlosanunciacao@hotmail.com>
2026-03-21 20:11:46 -0300

    up relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
- chromeWebSecurity: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter ',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: 'cypress/reports/junit/test-results-[hash].xml',
+      },
+  cypressMochawesomeReporterReporterOptions: {
+    charts: true,
+    reportPageTitle: 'Relatório de Testes',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  
+  }
+},
+chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
