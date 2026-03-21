fef478deec27e8958966174e5e6d71071a5b5578
Carlos Anunciacao <dev.carlosanunciacao@hotmail.com>
2026-03-21 19:41:41 -0300

    Up

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -4,11 +4,15 @@ describe('Carrinho', () => {
     cy.visit('https://www.saucedemo.com/');
 
     cy.get('#user-name').type('standard_user');
+
     cy.get('#password').type('secret_sauce');
+
     cy.get('[data-test="login-button"]').click();
 
     // act
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    cy.screenshot('carrinho-adicionado');
 
     // assert
     cy.get('.shopping_cart_badge')
@@ -18,6 +22,28 @@ describe('Carrinho', () => {
     cy.get('.shopping_cart_container').click();
 
     cy.contains('Sauce Labs Backpack').should('be.visible');
+
+    it('Remover produto do carrinho com sucesso', () => {
+
+    // Arrange
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('problem_user')
+
+    cy.get('[data-test="password"]').type('secret_sauce')
+    
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+    cy.get('.shopping_cart_badge')
+      .should('be.visible')
+
+    // Act
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+
+    cy.screenshot('produto removido')
+})
 
   });
   });
