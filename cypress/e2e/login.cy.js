68100f378e63886d94bc13eb93cc432232136708
Carlos Anunciação <155447042+CarlosAnunciacao@users.noreply.github.com>
2026-03-24 12:00:42 -0300

    up

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,25 +1,25 @@
 describe('Login', () => {
-  it('Realizar login com sucesso', () => {
-    // arrange
+  beforeEach(() => {
     cy.visit('https://www.saucedemo.com/');
-    // act
+  });
+
+  it('Realizar login com sucesso', () => {
+    // Act
     cy.get('#user-name').type('standard_user');
     cy.get('#password').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
 
-    // assert
+    // Assert
     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
   });
 
   it('Realizar login com falha', () => {
-    // arrange
-    cy.visit('https://www.saucedemo.com/');
-    // act
+    // Act
     cy.get('#user-name').type('user.invalid');
     cy.get('#password').type('senha_invalida');
     cy.get('[data-test="login-button"]').click();
 
-    //Assert
+    // Assert
     cy.get('[data-test="error"]')
         .should(
             'contain.text',
