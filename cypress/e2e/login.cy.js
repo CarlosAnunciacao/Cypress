472a8d22d6fbf1616582b82b981ee275913df94b
Carlos Anunciação <155447042+CarlosAnunciacao@users.noreply.github.com>
2026-03-30 08:39:22 -0300

    Att

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,28 +1,17 @@
+import LoginPage from '../support/pages/login.page';
+
 describe('Login', () => {
   beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/');
+    LoginPage.visit();
   });
 
   it('Realizar login com sucesso', () => {
-    // Act
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('[data-test="login-button"]').click();
-
-    // Assert
+    LoginPage.preencherLogin('standard_user', 'secret_sauce');
     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
   });
 
   it('Realizar login com falha', () => {
-    // Act
-    cy.get('#user-name').type('user.invalid');
-    cy.get('#password').type('senha_invalida');
-    cy.get('[data-test="login-button"]').click();
-
-    // Assert
-    cy.get('[data-test="error"]')
-        .should(
-            'contain.text',
-            'Epic sadface: Username and password do not match any user in this service');
+    LoginPage.preencherLogin('user.invalid', 'senha_invalida');
+    LoginPage.validarMensagemErro('Epic sadface: Username and password do not match any user in this service');
   }); 
 });
