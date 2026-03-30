643bd7c592763ff74627c6c05ae2c6aab8b7ab12
Carlos Anunciação <155447042+CarlosAnunciacao@users.noreply.github.com>
2026-03-30 08:41:20 -0300

    Att

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,17 +1,15 @@
-import LoginPage from '../support/pages/login.page';
 import InventoryPage from '../support/pages/inventory.page';
 
 describe('Carrinho', () => {
   beforeEach(() => {
-    LoginPage.visit();
-    LoginPage.preencherLogin('standard_user', 'secret_sauce');
+    // Usando o custom command criado
+    cy.login('standard_user', 'secret_sauce');
   });
 
   it('Adicionar produto ao carrinho com sucesso', () => {
     InventoryPage.adicionarMochila();
-    cy.screenshot('carrinho-adicionado');
-    
     InventoryPage.validarBadgeCarrinho(1);
+    
     InventoryPage.acessarCarrinho();
     InventoryPage.validarProdutoNoCarrinho('Sauce Labs Backpack');
   });
@@ -19,7 +17,6 @@ describe('Carrinho', () => {
   it('Remover produto do carrinho com sucesso', () => {
     InventoryPage.adicionarMochila();
     InventoryPage.removerMochila();
-    cy.screenshot('produto-removido');
     
     InventoryPage.validarBadgeCarrinho(0);
   });
